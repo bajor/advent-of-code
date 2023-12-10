1e539b4f25a13eacb6cfbccccf257ac9c02e3a6e
Maciek Bajor <maciekbajor@gmail.com>
2023-12-10 18:27:13 +0100

    working

--- day_5_part_2.py
+++ day_5_part_2.py
@@ -6,8 +6,8 @@ seeds = []
 # divide this into ranges for each map. Then map these ranges. Do this iteration for each map. 1 - see what maps we have - based on that divide our ranges to it. Map all ranges (shift values or leave if no corresponding map).
 
 
-with open("test.txt", "r") as file:
-# with open("day_5.txt", "r") as file:
+# with open("test.txt", "r") as file:
+with open("day_5.txt", "r") as file:
     for line in file:
         if line.strip():
             if line.lower().startswith("seeds:"):
@@ -22,7 +22,6 @@ with open("test.txt", "r") as file:
 
 seeds = [[seeds[i], seeds[i] + seeds[i + 1]] for i in range(0, len(seeds) - 1, 2)]
 
-# print(seeds)
 
 def map_raw_lists_to_ranges(numbers):
     for i in range(len(numbers)):
@@ -37,26 +36,54 @@ def map_raw_lists_to_ranges(numbers):
     return numbers
 
 
-def perform_mapping(current_range, current_stage_maps):
+def perform_mapping_single_number(source_number, current_stage_maps):
+    found_map = None
+
     for current_map in current_stage_maps[1]:
         min_value = min(current_map[1])
         max_value = max(current_map[1])
 
-        if (current_range[0] >= min_value and current_range[0] <= max_value) or (current_range[1] >= min_value and current_range[1] <= max_value):
-            source_map = current_map[1]
-            target_map = current_map[0]
+        if source_number >= min_value and source_number <= max_value:
+            found_map = current_map
 
-            difference_from_start = current_range[0] - min(source_map)
-            current_range_len = current_range[1] - current_range[0]
+    if not found_map:
+        return source_number
 
-            target_start = target_map[0] + difference_from_start
-            target_end = target_start + current_range_len
+    source_map = found_map[1]
+    target_map = found_map[0]
 
-            return [target_start, target_end]
+    difference_from_start = source_number - min(source_map)
+    target_number = min(target_map) + difference_from_start
+    return target_number
+
+
+def perform_mapping(current_range, current_stage_maps):
+
+    for current_map in current_stage_maps[1]:
+        min_value = min(current_map[1])
+        max_value = max(current_map[1])
+
+        # print("current range ", current_range)
+
+        if isinstance(current_range, list):
+            if (current_range[0] >= min_value and current_range[0] <= max_value) or (current_range[1] >= min_value and current_range[1] <= max_value):
+                source_map = current_map[1]
+                target_map = current_map[0]
+
+                difference_from_start = current_range[0] - min(source_map)
+                current_range_len = current_range[1] - current_range[0]
+
+                target_start = target_map[0] + difference_from_start
+                target_end = target_start + current_range_len
+
+                return [target_start, target_end]
     return current_range
 
 
 def cut_list(current_range, map_ranges):
+    if isinstance(current_range, int):
+        return current_range
+
     start = current_range[0] 
     end = current_range[1] 
     breaking_start = []
@@ -69,86 +96,59 @@ def cut_list(current_range, map_ranges):
         for p in c:
             breaking_points.append(p)
 
-    # Ensure that the start is less than or equal to the end
     if start > end:
+        # print(current_range, map_ranges, start,  end)
+
         raise ValueError("Start must be less than or equal to end")
 
-    # Sort breaking points in ascending order
     breaking_points.sort()
-
-    # Initialize a list to store the split ranges
     split_ranges = []
-
-    # Initialize variables to keep track of the current start and end of the range
     current_start = start
     current_end = end
 
-    # Iterate through breaking points
     for point in breaking_points:
-        # Check if the breaking point is within the current range
         if current_start <= point <= current_end:
-            # Split the range at the breaking point
             split_ranges.append([current_start, point])
             current_start = point
-
-    # Add the remaining part of the range, if any
     if current_start < current_end:
         split_ranges.append([current_start, current_end])
-
     for i in range(len(split_ranges)):
-
-        if split_ranges[i][0] in breaking_end:
+        if (split_ranges[i][0] in breaking_end) and (split_ranges[i][0] < split_ranges[i][1]):
             split_ranges[i][0] = split_ranges[i][0] + 1
-
-        if split_ranges[i][1] in breaking_start:
+        if (split_ranges[i][1] in breaking_start) and (split_ranges[i][1] > split_ranges[i][0]):
             split_ranges[i][1] = split_ranges[i][1] - 1
-
         if split_ranges[i][0] == split_ranges[i][1]:
             split_ranges[i] = split_ranges[i][0]
 
     return split_ranges
+
 
 def perform_complete_chain_of_mapping(ranges, maps):
     for current_map in maps:
         current_cut_map = current_map[1]
         current_cut_map = [x[1] for x in current_cut_map]
 
-        # print("current map ", current_map)
-        # print("current ranges ", ranges)
-
-        def perform_stage(ranges, current_map):
+        def perform_stage_mapping(ranges, current_map):
             results = []
             for current_range in ranges:
                 cut_ranges = cut_list(current_range, current_cut_map)
 
-                print("cut ranges", cut_ranges)
-
-                for cut_range in cut_ranges:
-
-                    print("cut_range, current map ", cut_range, current_map)
-
-                    result = perform_mapping(cut_range, current_map)
+                if isinstance(current_range, int):
+                    result = perform_mapping_single_number(current_range, current_map)
                     results.append(result)
+                else:
+                    for cut_range in cut_ranges:
+                        result = perform_mapping(cut_range, current_map)
+                        results.append(result)
             return results
 
 
-        print("ranges before mapping ", ranges)
+        ranges = perform_stage_mapping(ranges, current_map) 
 
-        ranges = perform_stage(ranges, current_map) 
+        # print("ranges after stage mapping, ", ranges)
 
-        print("ranges after mapping ", ranges)
-
-
-        1/0
-
-            # for range_
-
-            # TODO: now perform mapping. If range not in map - return as is
-
-
-
-    # 1/0
-
+        # 1/0
+    return ranges
 
 maps = [
     [header, map_raw_lists_to_ranges(number_list)]
@@ -156,17 +156,23 @@ maps = [
 ]
 
 
-
-result = None 
-
 output = perform_complete_chain_of_mapping(seeds, maps)
-# for seed_range in seeds:
-    # if result is None:
-    #     result = output
-    # if output < result:
-    #     result = output
+
+numbers = []
+
+for o in output:
+    if isinstance(o, int):
+        numbers.extend([o])
+    else:
+        numbers.extend(o)
+
+# print(numbers)
 
 
-# print(result)
+numbers = sorted(numbers)
 
 
+print(numbers)
+# print(min(numbers))
+
+# print(output)

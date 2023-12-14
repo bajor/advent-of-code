6c3357c24691fbfb45aa4be62fad47636b838f2f
Maciek Bajor <maciekbajor@gmail.com>
2023-12-14 23:48:07 +0100

    wip

--- day_7_part_1.py
+++ day_7_part_1.py
@@ -1,9 +1,10 @@
 import re
 
 
-# with open("day_7.txt") as file:
-with open("test.txt") as file:
+with open("day_7.txt") as file:
+# with open("test.txt") as file:
     lines = [x.strip() for x in file]
+
 
 def is_five_of_kind(cards):
     score = cards[1]
@@ -15,7 +16,6 @@ def is_five_of_kind(cards):
 def is_four_of_kind(cards):
     score = cards[1]
     cards = cards[0]
-
     if (cards.count(cards[0]) == 4):
         remaining = [x for x in cards if x != cards[0]]
         return (cards[0], remaining[0]), score
@@ -101,6 +101,8 @@ def rank_card(card):
         return 12 
     elif card == "J":
         return 11 
+    elif card == "T":
+        return 10 
     elif card.isdigit():
         return int(card)
 
@@ -137,41 +139,62 @@ for i in range(len(lines)):
         continue
     high_card.append(lines[i])
 
-result = []
-
-five_of_kind = [(rank_card(x[0]), x[1]) for x in five_of_kind]
-five_of_kind = sorted(five_of_kind, key=lambda x: x[0], reverse=True)
-five_of_kind = [x[-1] for x in five_of_kind]
-result.extend(five_of_kind)
-
-four_of_kind = [(rank_card(x[0][0]), rank_card(x[0][1]), x[1]) for x in four_of_kind]
-four_of_kind = sorted(four_of_kind, key=lambda x: (x[0], x[1]), reverse=True)
-four_of_kind = [x[-1] for x in four_of_kind]
-result.extend(four_of_kind)
-
-three = [(rank_card(x[0]), sorted((rank_card(x[1][0]), rank_card(x[1][1])), reverse=True),  x[2]) for x in three]
-three = [(x[0], x[1][0], x[1][1], x[2]) for x in three]
-three = sorted(three, key=lambda x: (x[0], x[1], x[2]), reverse=True)
-three = [x[-1] for x in three]
-result.extend(three)
-
-two_pairs = [(sorted((rank_card(x[0][0]), rank_card(x[0][1])), reverse=True), rank_card(x[1]), x[2]) for x in two_pairs]
-two_pairs = [(x[0][0], x[0][1], x[1], x[2]) for x in two_pairs]
-two_pairs = sorted(two_pairs, key=lambda x: (x[0], x[1], x[2]), reverse=True)
-two_pairs = [x[-1] for x in two_pairs]
-result.extend(two_pairs)
-
-one_pair = [(rank_card(x[0]), sorted((rank_card(x[1][0]), rank_card(x[1][1]), rank_card(x[1][2])), reverse=True), x[2]) for x in one_pair]
-one_pair = [(x[0], x[1][0], x[1][1], x[1][2], x[2]) for x in one_pair]
-one_pair = sorted(one_pair, key=lambda x: (x[0], x[1], x[2], x[3]), reverse=True)
-one_pair = [x[-1] for x in one_pair]
-result.extend(one_pair)
+ranking = []
 
 high_card = [(list(x[0]), x[1]) for x in high_card]
 high_card = [([rank_card(y) for y in x[0]], x[1]) for x in high_card]
 high_card = [(sorted(x[0], reverse=True), x[1]) for x in high_card]
 high_card = [(x[0][0], x[0][1], x[0][2], x[0][3], x[0][4], x[1]) for x in high_card]
-high_card = sorted(high_card, key=lambda x: (x[0], x[1], x[2], x[3], x[4]), reverse=True)
+high_card = sorted(high_card, key=lambda x: (x[0], x[1], x[2], x[3], x[4]) )
 high_card = [x[-1] for x in high_card]
-result.extend(high_card)
+ranking.extend(high_card)
 
+one_pair = [(rank_card(x[0]), sorted((rank_card(x[1][0]), rank_card(x[1][1]), rank_card(x[1][2])), reverse=True), x[2]) for x in one_pair]
+one_pair = [(x[0], x[1][0], x[1][1], x[1][2], x[2]) for x in one_pair]
+one_pair = sorted(one_pair, key=lambda x: (x[0], x[1], x[2], x[3]) )
+one_pair = [x[-1] for x in one_pair]
+ranking.extend(one_pair)
+
+two_pairs = [(sorted((rank_card(x[0][0]), rank_card(x[0][1])), reverse=True), rank_card(x[1]), x[2]) for x in two_pairs]
+two_pairs = [(x[0][0], x[0][1], x[1], x[2]) for x in two_pairs]
+two_pairs = sorted(two_pairs, key=lambda x: (x[0], x[1], x[2]))
+two_pairs = [x[-1] for x in two_pairs]
+ranking.extend(two_pairs)
+
+three = [(rank_card(x[0]), sorted((rank_card(x[1][0]), rank_card(x[1][1])), reverse=True),  x[2]) for x in three]
+three = [(x[0], x[1][0], x[1][1], x[2]) for x in three]
+three = sorted(three, key=lambda x: (x[0], x[1], x[2]))
+three = [x[-1] for x in three]
+ranking.extend(three)
+
+full_house = [(rank_card(x[0][0]), rank_card(x[0][1]),  x[1]) for x in full_house]
+full_house = sorted(full_house, key=lambda x: (x[0], x[1]))
+full_house = [x[-1] for x in full_house]
+ranking.extend(full_house)
+
+four_of_kind = [(rank_card(x[0][0]), rank_card(x[0][1]), x[1]) for x in four_of_kind]
+four_of_kind = sorted(four_of_kind, key=lambda x: (x[0], x[1]) )
+four_of_kind = [x[-1] for x in four_of_kind]
+ranking.extend(four_of_kind)
+
+five_of_kind = [(rank_card(x[0]), x[1]) for x in five_of_kind]
+five_of_kind = sorted(five_of_kind, key=lambda x: x[0])
+five_of_kind = [x[-1] for x in five_of_kind]
+ranking.extend(five_of_kind)
+
+final_score = 0
+# print(len(ranking))
+
+for i in range(len(ranking)):
+    rank = i + 1
+    score = rank * ranking[i]
+    final_score += score
+
+
+print(final_score)
+# 199657538
+# 200787843 - too low
+# 250263225 - too low
+
+for r in ranking:
+    print(r)

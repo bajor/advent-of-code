3e3f6d1e3fc0ee9b59caa5d95d39b09473eae779
Maciek Bajor <maciekbajor@gmail.com>
2023-12-22 21:38:57 +0100

    clean up

--- day_12_part_1.py
+++ day_12_part_1.py
@@ -12,19 +12,18 @@ def generate_combinations(n, k):
 
 
 def generate_possible_positions(q_positions, missing_hashes):
+    q_positions_map = [[x, p] for x, p in zip(range(len(q_positions)), q_positions)]
+    q_positions_map = {x[0]: x[1] for x in q_positions_map}
+
     n = len(q_positions)
     k = missing_hashes
 
     combinations_ids = generate_combinations(n, k)
     combinations_ids = [list(x) for x in combinations_ids]
 
-    q_positions_map = [[x, p] for x, p in zip(range(len(q_positions)), q_positions)]
-    q_positions_map = {x[0]: x[1] for x in q_positions_map}
-
     for i in range(len(combinations_ids)):
         for j in range(len((combinations_ids[i]))):
             combinations_ids[i][j] = q_positions_map[combinations_ids[i][j]]
-
     return combinations_ids
 
 
@@ -35,11 +34,10 @@ def count_correct_lines(possible_lines, contiguous_groups):
         grouped_periods = ["".join(g) for k, g in groupby(line) if k == "#"]
         grouped_periods = [len(x) for x in grouped_periods]
         validated.append(grouped_periods)
-
     return validated.count(contiguous_groups)
 
 
-possible_positions_count = 0
+all_correct_possibilities = 0
 
 for l in lines:
     q_positions = [i for i, c in enumerate(l) if c == "?"]
@@ -61,6 +59,6 @@ for l in lines:
         line = line.replace("?", ".")
         possible_lines.append(line)
 
-    possible_positions_count += count_correct_lines(possible_lines, contiguous_groups)
+    all_correct_possibilities += count_correct_lines(possible_lines, contiguous_groups)
 
-print(possible_positions_count)
+print(all_correct_possibilities)

e15e95490e8d1ccaeeebcda01d6c6b735c3f76e8
Maciek Bajor <maciekbajor@gmail.com>
2023-12-24 14:07:35 +0100

    wip

--- day_13_part_1.py
+++ day_13_part_1.py
@@ -1,4 +1,5 @@
-with open("test.txt") as file:
+# with open("test.txt") as file:
+with open("day_13.txt") as file:
     lines = [x.strip() for x in file]
 
 
@@ -13,13 +14,11 @@ def create_vertical_lists(lines):
 
 
 def generate_split_pairs(lines):
-    # return list of pairs to check 
     pairs = []
 
-    for split in range(1, len(lines)):
+    for split in range(0, len(lines)):
         first = lines[:split]
         second = lines[split:]
-
         size = min(len(first), len(second))
 
         if size == 1:
@@ -30,28 +29,59 @@ def generate_split_pairs(lines):
             second = second[:2]
 
         pairs.append([first, second])
-        
     return pairs
 
 
 def find_mirrors(pairs):
-    for pair in pairs:
-        first_lines, second_lines = pair 
-
+    for i in range(len(pairs)):
+        first_lines, second_lines = pairs[i] 
         second_lines = list(reversed(second_lines))
 
-        print(first_lines, second_lines)
+        if first_lines == second_lines:
+            return i
+    return 0
 
 
-
-horizontal = lines
-vertical = create_vertical_lists(lines)
-
-pairs_to_check = generate_split_pairs(horizontal)
-
-find_mirrors(pairs_to_check)
+def find_mirror_position(lines):
+    pairs_to_check = generate_split_pairs(lines)
+    split_position = find_mirrors(pairs_to_check)
+    return split_position
 
 
-# for p in pairs_to_check:
-    # print(p)
+def calculate_puzzle(lines):
+    horizontal = lines
+    vertical = create_vertical_lists(lines)
 
+    mirror_position_v = find_mirror_position(vertical)
+    mirror_position_h = find_mirror_position(horizontal)
+
+    result = mirror_position_h * 100 + mirror_position_v
+    return result
+
+
+def split_lines_into_puzzles(lines):
+    cumulated = []
+    puzzles = []
+
+    for l in lines:
+        if l == "":
+            puzzles.append(cumulated) 
+            cumulated = []
+        else:
+            cumulated.append(l)
+    puzzles.append(cumulated)
+    return puzzles 
+
+
+puzzles = split_lines_into_puzzles(lines)
+total = 0
+
+print(puzzles)
+
+for p in puzzles:
+    total += calculate_puzzle(p)
+
+print(total)
+
+# 5334 - too low 
+# 28530 - too high

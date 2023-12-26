a4a24e62392430783d7773f75a7570192b8790ba
Maciek Bajor <maciekbajor@gmail.com>
2023-12-26 15:54:36 +0100

    wip

--- day_13_part_1.py
+++ day_13_part_1.py
@@ -1,12 +1,14 @@
-import copy
+import copy 
 
-# with open("test.txt") as file:
-with open("day_13.txt") as file:
+
+with open("test.txt") as file:
+# with open("day_13.txt") as file:
     lines = [x.strip() for x in file]
 
 
 def create_vertical_lists(lines):
     vertical = []
+
     for index in range(len(lines[0])):
         vert = []
         for line in lines:
@@ -19,20 +21,32 @@ def generate_split_pairs(lines):
     pairs = []
 
     for split in range(0, len(lines)):
-        first = copy.deepcopy(lines[:split])
-        second = copy.deepcopy(lines[split:])
+        first = lines[:split]
+        second = lines[split:]
         size = min(len(first), len(second))
 
         if size == 1:
             first = first[-1:]
             second = second[:1]
-            lines = ["".join(l) for l in lines]
+
+            # if len(lines) > 1:
+            first = ["".join(l) for l in first]
+            second = ["".join(l) for l in second]
+
         else:
             first = first[-2:]
             second = second[:2]
 
+            first = ["".join(l) for l in first]
+            second = ["".join(l) for l in second]
+            # if len(lines) > 1:
+            #     lines = ["".join(l) for l in lines]
+
         if first and second:
             pairs.append([first, second])
+
+    # print(pairs)
+    # print(" ")
 
     return pairs
 
@@ -59,6 +73,17 @@ def calculate_puzzle(lines):
 
     mirror_position_v = find_mirror_position(vertical)
     mirror_position_h = find_mirror_position(horizontal)
+
+
+    if mirror_position_h == 0 and mirror_position_v == 0:
+        # print(lines)
+        for l in lines:
+            print("".join(l))
+
+        print("    ")
+
+    # print(mirror_position_v, mirror_position_h)
+
 
     result = mirror_position_h + mirror_position_v * 100
     return result
@@ -91,3 +116,7 @@ print(total)
 # 5334 - too low 
 # 28530 - too high
 # 22972 - not correcct
+
+# 27505 correct
+
+### !!! some pairs are as list but most are as strings !!!

cd44fb355dc356628fda4699770ed99c1cfb5956
Maciek Bajor <maciekbajor@gmail.com>
2023-12-31 21:28:29 +0100

    clean up

--- day_15_part_1.py
+++ day_15_part_1.py
@@ -2,18 +2,18 @@ with open("day_15.txt") as file:
     values = file.read().split(",")
 
 
-def calculate_hash(char, value=0):
+def calculate_hash_char(char, value=0):
     char = ord(char)
     char = (char + value) * 17
     return char % 256
 
 
-def calculate_list_hash(chars):
+def calculate_hash_list(chars):
     result = 0
     for c in chars:
-        result = calculate_hash(c, result)
+        result = calculate_hash_char(c, result)
     return result
 
 
-values = [calculate_list_hash(x) for x in values]
+values = [calculate_hash_list(x) for x in values]
 print(sum(values))

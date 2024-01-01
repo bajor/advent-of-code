58a61a582b3a8039b0330972bf094e35f6599238
Maciek Bajor <maciekbajor@gmail.com>
2024-01-02 00:21:17 +0100

    wip

--- day_16_part_1.py
+++ day_16_part_1.py
@@ -2,38 +2,54 @@
 with open("test.txt") as file:
     lines = [x.strip() for x in file]
 
-# input as hashmap x,y and symbol
-# direction as hashmap - what to add to go in that direction
-# function to move - add direction + current coords (add two tuples)     
-
-# going to symbol - pass direction where you come from and 
 
 MAX_ROW = len(lines)
 MAX_COL = len(lines[0])
 
-
-move = {
-    "up" : (0,-1),
-    "down" : (0,1),
-    "right" : (1,0),
-    "left" : (-1,0)
-}
+MOVE_DIRS = {
+    "up": (0, -1), 
+    "down": (0, 1), 
+    "right": (1, 0), 
+    "left": (-1, 0)
+    }
 
 
 def create_grid_map(lines):
     grid = {}
     for y in range(len(lines)):
         for x in range(len(lines[y])):
-            grid[(x,y)] = r"" + lines[y][x]
+            grid[(x, y)] = lines[y][x]
     return grid
 
 
-def perform_move(current_pos, direction):
-    direction = move[direction]
-    return (current_pos[0] + direction[0], current_pos[1] + direction[1])
+def perform_walk(current_coords, direction, history):
+    direction = MOVE_DIRS[direction]
+
+    new_x = current_coords[0] + direction[0]
+    new_y = current_coords[1] + direction[1]
+
+    if new_x < 0 or new_x > MAX_ROW:
+        all_walks_histories.append(history)
+        return
+
+    if new_y < 0 or new_y > MAX_COL:
+        all_walks_histories.append(history)
+        return
+
+    # logic for next direction, check what char are we on, return direction
+    next_direction = "down"
+
+    history.append((new_x, new_y))
+    perform_walk((new_x, new_y), next_direction, history)
 
 
 grid = create_grid_map(lines)
 
-a = perform_move((1,1), "up")
-print(a)
+start_position = (0,0)
+start_direction = "right"
+
+all_walks_histories = []
+
+perform_walk(start_position, start_direction, [])
+
+print(all_walks_histories)
